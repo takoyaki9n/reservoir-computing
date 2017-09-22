9850402e28a4e3858b910b8047900611dcc43388
Yahiro Wataru <wtr816@gmail.com>
2017-09-22 15:47:14 +0900

    refactor in tests

--- java/DNAReservoir/src/test/RandominputTest.java
+++ java/DNAReservoir/src/test/RandominputTest.java
@@ -5,7 +5,7 @@ import main.Main;
 
 public class RandominputTest {
 	static public void run() {
-		Input input = Input.generateInput(Main.config);
+		Input input = Input.generateInput(Main.manager);
 		for (int i = 0; i < input.getData().size(); i++) {
 			System.out.println(i + " " + input.get(i));
 		}
--- java/DNAReservoir/src/test/TaskTest.java
+++ java/DNAReservoir/src/test/TaskTest.java
@@ -8,9 +8,9 @@ import task.Task;
 
 public class TaskTest {
 	static public void run() {
-		Input input = Input.generateInput(Main.config);
-		ArrayList<Task> tasks = Task.generateTasks(input, Main.config);
-		for (int i = 0; i < Main.config.simulationTime; i++) {
+		Input input = Input.generateInput(Main.manager);
+		ArrayList<Task> tasks = Task.generateTasks(input, Main.manager);
+		for (int i = 0; i < Main.manager.simulationTime; i++) {
 			System.out.println(i + " " + input.get(i) + " " + tasks.get(0).get(i) + " " + tasks.get(1).get(i));
 		}
 	}
