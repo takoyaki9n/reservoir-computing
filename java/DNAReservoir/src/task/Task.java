1240b7e95f531578a542aa5a865ee5577de342a9
Yahiro Wataru <wtr816@gmail.com>
2017-09-27 11:49:01 +0900

    implement Task.getDataAsArray

--- java/DNAReservoir/src/task/Task.java
+++ java/DNAReservoir/src/task/Task.java
@@ -9,13 +9,22 @@ import javax.json.JsonObject;
 import input.Input;
 
 public class Task {
-	protected ArrayList<Double> data;
-	
 	public int start, end, length;
+	
+	//TOTO: to double[]
+	protected ArrayList<Double> data;
 	
 	public double get(int i) { return data.get(i); }
 	
 	public ArrayList<Double> getData(){ return data; }
+	
+	public double[] getDataAsArray() {
+		double[] array = new double[length];
+		for (int i = 0; i < length; i++) {
+			array[i] = data.get(i).doubleValue();
+		}
+		return array;
+	}
 	
 	static private Task generateTask(JsonObject taskConfig, HashMap<String, Input> inputs) {
 		String type = taskConfig.getString("type");
