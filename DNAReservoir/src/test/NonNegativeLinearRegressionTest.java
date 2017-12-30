0bd2f937db0a8d45f13854c6ba469a8d6365546c
Yahiro Wataru <wtr816@gmail.com>
2017-12-30 17:24:43 +0900

    noneg test

--- DNAReservoir/src/test/NonNegativeLinearRegressionTest.java
+++ DNAReservoir/src/test/NonNegativeLinearRegressionTest.java
@@ -1,24 +1,72 @@
 package test;
 
+import org.apache.commons.math3.special.BesselJ;
+
+import com.google.common.annotations.Beta;
+
 import util.MyOLSMultipleNonNegativeLinearRegression;
 
 public class NonNegativeLinearRegressionTest {
 	public static void main(String[] args) {	
-		MyOLSMultipleNonNegativeLinearRegression regression = new MyOLSMultipleNonNegativeLinearRegression();
-		
 		double[] y = new double[] { 0.8587, 0.1781, 0.0747, 0.8405 };
-
 		double[][] x = new double[4][];
 		x[0] = new double[] { 0.0372, 0.2869 };
 		x[1] = new double[] { 0.6861, 0.7071 };
 		x[2] = new double[] { 0.6233, 0.6245 };
 		x[3] = new double[] { 0.6344, 0.6170 };
-		regression.setNoIntercept(true);
-		regression.newSampleData(y, x);
-
-		double[] beta = regression.estimateRegressionParameters();
-		for (double p : beta) {
-		    System.out.println(p);
+		test(x, y, true);
+		
+		int n = 4, m = 10;
+		double[] b = new double[n];
+		for (int i = 0; i < n; i++) b[i] = Math.random() * 100.0;
+		
+		y = new double[m];
+		x = new double[m][];
+		for (int i = 0; i < m; i++) {
+			x[i] = new double[n - 1];
+			y[i] = b[0];
+			for (int j = 0; j < n - 1; j++) {
+				x[i][j] = (2.0 * Math.random() - 1.0) * 100.0;
+				y[i] += x[i][j] * b[j + 1];
+			}
+//			y[i] *= 1.0 + (2.0 * Math.random() - 1.0) * 1.0e-3;
 		}
+		System.out.println("\ntrue beta");
+		for (double p : b) System.out.println(p);
+		test(x, y, false);
+		
+		b = new double[n];
+		for (int i = 0; i < n - 1; i++) b[i] = Math.random() * 100.0;
+		b[n - 1] = -Math.random() * 5.0;
+				
+		y = new double[m];
+		x = new double[m][];
+		for (int i = 0; i < m; i++) {
+			x[i] = new double[n - 1];
+			y[i] = b[0];
+			for (int j = 0; j < n - 1; j++) {
+				x[i][j] = (2.0 * Math.random() - 1.0) * 100.0;
+				y[i] += x[i][j] * b[j + 1];
+			}
+//			y[i] *= 1.0 + (2.0 * Math.random() - 1.0) * 1.0e-3;
+		}
+		System.out.println("\ntrue beta");
+		for (double p : b) System.out.println(p);
+		test(x, y, false);
+	}
+	
+	private static void test(double[][] x, double[] y, boolean noIntercept) {
+		MyOLSMultipleNonNegativeLinearRegression regression = new MyOLSMultipleNonNegativeLinearRegression();
+		regression.setNoIntercept(noIntercept);
+		regression.newSampleData(y, x);
+		
+		double[] beta = regression.estimateRegressionParameters();
+		System.out.println("beta");
+		for (double p : beta) System.out.println(p);
+		
+		double[] z = regression.calculateEstimatedValues();
+		System.out.println("y z");
+		for (int i = 0; i < y.length; i++) System.out.println(y[i] + " " + z[i]);
+		System.out.println("NRMSE \n" + regression.calculateNRMSE());
 	}
 }
