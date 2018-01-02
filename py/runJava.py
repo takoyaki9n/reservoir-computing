5a0059b1076ef229224bb764f4f85ae1a8fdf1c6
Yahiro Wataru <wtr816@gmail.com>
2018-01-02 15:36:43 +0900

    update runJava.py

--- py/runJava.py
+++ py/runJava.py
@@ -4,18 +4,17 @@ import os
 from config import *
 
 if __name__ == "__main__":
-    f = open(casesDir + "/osc3_10_0/config.json")
+    f = open(casesDir + "/" + pref + "/config.json")
     template = json.load(f)
     f.close()
     for mx in maxs:
         for tau in taus:
-            caseDir = casesDir + "/%s_%s_%s" % (pref, tau, mx)
+            caseDir = casesDir + "/%s/%s_%s" % (pref, tau, mx)
             if not os.path.isdir(caseDir):
                 os.mkdir(caseDir)
             template["inputs"][0]["max"] = maxVals[mx]
             template["inputs"][0]["interval"] = int(tau)
             template["tasks"][1]["interval"] = int(tau)
-            print(template)
 
             conf = caseDir + "/config.json"
             json.dump(template, open(conf, "w"))
