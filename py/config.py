bc15fa5ab399451b8f74b8a5fe568885d351ea16
Yahiro Wataru <wtr816@gmail.com>
2018-01-02 17:54:29 +0900

    ecit config.py

--- py/config.py
+++ py/config.py
@@ -1,10 +1,11 @@
+import sys
 from os.path import abspath, dirname
 
 baseDir = dirname(dirname(abspath(__file__)))
 casesDir = baseDir + "/cases"
 tmpDir = baseDir + "/tmp"
 
-pref = "osc3"
+pref = sys.argv[1]
 taus = ["10", "25", "50", "100"]
 maxs = ["01", "025", "05", "1", "2"]
 maxVals = {"01": 0.1, "025": 0.25, "05": 0.5, "1": 1.0, "2": 2.0}
