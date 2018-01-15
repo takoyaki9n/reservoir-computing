2427023a774f84293824df12330ad6a20666baf5
Yahiro Wataru <wtr816@gmail.com>
2018-01-15 15:36:12 +0900

    TaskController

--- DNAReservoir/src/task/TaskController.java
+++ DNAReservoir/src/task/TaskController.java
@@ -10,7 +10,7 @@ public class TaskController extends Task {
 	protected Input input;
 	
 	private double alpha;
-	private int tau;
+	private int delta;
 
 	public TaskController(JsonObject config, HashMap<String, Input> inputs) {
 		super(config);
@@ -21,35 +21,29 @@ public class TaskController extends Task {
 		length = input.end;
 		
 		alpha = config.getJsonNumber("alpha").doubleValue();
-		tau = config.getInt("tau");
+		delta = config.getInt("delta");
 		
 		initializeData();
 	}
 
-private void initializeData() {
-	data = new double[length];
-	
-	double r = 0.0;
-	int c = 0, q = 0;
-	for (int t = 0; t < length; t++) {
-		if (t == start) 
-			r = input.get(t);
-		else if (start < t && t < end) {
-			if (q == 0) {
-				if (input.get(t) + alpha <= r) {
-					q = 1;
-					c = 0;
+	private void initializeData() {
+		data = new double[length];
+		
+		double m = input.get(start);
+		int q = 0, u = 0;
+		for (int t = 0; t < length; t++) {
+			data[t] = (q == 0)? 0.0: 1.0;
+			if (start <= t && t < end) {
+				if (q == 0) {
+					if (input.get(t) + alpha < m) {
+						q = 1; u = t;
+					}
+				} else {
+					if (t - u >= delta) {
+						q = 0; m = input.get(t);
+					}
 				}
-			} else {
-				if (c >= tau) {
-					q = 0;
-					r = input.get(t);
-				}
-				c++;
 			}
-		}
-		data[t] = (q == 0)? 0.0: 1.0;
-	}		
-}
-
+		}		
+	}
 }
